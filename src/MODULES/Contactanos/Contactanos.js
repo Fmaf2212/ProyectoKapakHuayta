5ac0e7cf5b1065eb6f87c1e4f7ac382b8cbef83a
moises <fmaf_3011@outlook.com>
2022-08-08 00:55:59 -0500

    añadiendo página Contactanos y modificando titulo del footer

--- src/MODULES/Contactanos/Contactanos.js
+++ src/MODULES/Contactanos/Contactanos.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import './contactanos.css'
 
+import imagenDefault from '../../BASE/img/imagenDefault.png'
 
 const Contactanos = () => {
   return (
-    <div className='contactanos'>Contáctanos</div>
+    <div className='containContactanos'>
+        <section className='containContactanos__image'>
+            <h2>Lorem Ipsum Simply dummy text</h2>
+            <img src={imagenDefault} alt="imagenDefault" />
+        </section>
+        <section className='containContactanos__form'>
+            <h2>Estamos para apoyarte</h2>
+            <h1>Formulario de contacto</h1>
+        </section>
+    </div>
   )
 }
 
